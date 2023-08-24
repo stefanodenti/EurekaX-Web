55d50b3878aa46df760eaf78257aa62bd395c352
Stefano <stefanodenti4@gmail.com>
2023-08-24 17:36:21 +0200

    standalone pages

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,10 +1,16 @@
+import { CommonModule } from '@angular/common';
 import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'eurekax-home',
   templateUrl: './home.component.html',
-  styleUrls: ['./home.component.scss']
+  styleUrls: ['./home.component.scss'],
+  standalone: true,
+  imports: [
+    CommonModule
+  ]
 })
+
 export class HomeComponent implements OnInit {
 
   constructor() { }

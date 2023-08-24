7efcd68fc9590e0625ee4322967fe4c155087d64
Stefano <stefanodenti4@gmail.com>
2023-08-24 17:32:13 +0200

    cli16

--- src/app/pages/contacts/contacts.component.ts
+++ src/app/pages/contacts/contacts.component.ts
@@ -1,9 +1,17 @@
+import { CommonModule } from '@angular/common';
 import { Component, OnInit } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   templateUrl: './contacts.component.html',
   styleUrls: ['./contacts.component.scss'],
+  standalone: true,
+  imports: [
+    CommonModule,
+    FormsModule
+  ]
 })
+
 export class ContactsComponent implements OnInit {
   email = "hello@email.com"
   constructor() { }

a76171c3fb2cebd37fccebbda608128b4d59268b
onzomarco <marco.onzo@k-sport.tech>
2023-08-29 18:35:45 +0200

    aggiunto theme service

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,8 +2,16 @@ module.exports = {
   content: [
     "./src/**/*.{html,ts}",
   ],
+  darkMode: "class",
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        "primary": "var(--primary-color)",
+        "secondary": "var(--secondary-color)",
+        "tertiary": "var(--tertiary-color)",
+        "accent": "var(--accent-color)"
+      },
+    },
   },
   plugins: [],
 }

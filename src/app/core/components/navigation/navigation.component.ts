85cd80bc80af5172492e178d934b99bfc5c24a1e
onzomarco <marco.onzo@k-sport.tech>
2023-09-07 17:54:54 +0200

    cia waglio

--- src/app/core/components/navigation/navigation.component.ts
+++ src/app/core/components/navigation/navigation.component.ts
@@ -1,7 +1,8 @@
 import {Component, Input} from '@angular/core';
 import {Navigation} from "../../models/config.model";
 import {CommonModule} from "@angular/common";
-import {RouterModule} from "@angular/router";
+import {ActivatedRoute, Router, RouterModule} from "@angular/router";
+import {NotificationType} from "../../models/notification.model";
 
 @Component({
   selector: 'eurekax-navigation',
@@ -13,4 +14,21 @@ import {RouterModule} from "@angular/router";
 export class NavigationComponent {
   @Input() navigations: Navigation[] | null = null;
   @Input() mode: 'vertical' | 'horizontal' = 'horizontal';
+
+  constructor(private activatedRoute: ActivatedRoute, private router: Router) {
+    console.log('NAVIGATION', this.activatedRoute.snapshot);
+    console.log(this.router.url)
+  }
+
+
+  getCssClass(navigation: Navigation) {
+    let classesObject: { [p: string]: boolean } = {};
+    if (!!navigation.cssClass) {
+      classesObject[navigation.cssClass] = !!navigation.cssClass
+    }
+    return {
+      ...classesObject,
+      'text-primary': navigation.routerLink === this.router.url || navigation.childs?.find(nav => nav.routerLink === this.router.url)
+    };
+  }
 }

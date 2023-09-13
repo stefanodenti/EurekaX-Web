22d42a7c44c143d9e5c0d8e4db037c7a9f508248
Stefano <stefanodenti4@gmail.com>
2023-09-13 12:32:36 +0200

    save

--- src/app/auth/pages/auth-manager/auth-manager.component.ts
+++ src/app/auth/pages/auth-manager/auth-manager.component.ts
@@ -1,13 +1,16 @@
 import { Component } from '@angular/core';
-import {Action} from "../../models/user";
+import { Action } from '../../models/user';
+import { QueryFirestoreService } from 'src/app/core/services/query-firestore.service';
 
 @Component({
   templateUrl: './auth-manager.component.html',
-  styleUrls: ['./auth-manager.component.scss']
+  styleUrls: ['./auth-manager.component.scss'],
 })
 export class AuthManagerComponent {
-
+  constructor(private queryService: QueryFirestoreService) {}
   saveAction(action: Action) {
-    console.log(action)
+    this.queryService.create(action, 'auth-actions').then((res) => {
+      console.warn('ADD ACTION RES: ', res);
+    });
   }
 }
